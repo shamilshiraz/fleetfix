301aa9c14479acc72c67b8a242c98a7e5fe8f1d4
shamilshiraz <shamilshiraz0@gmail.com>
2025-01-19 02:39:31 +0530

    added arabic

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,26 +1,32 @@
-import { motion } from 'framer-motion';
-import React from 'react'
+import { motion, useScroll, useTransform } from 'framer-motion';
+import React, { useRef } from 'react'
 import { useTranslation } from 'react-i18next';
 
 function About() {
+  const container = useRef();
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ['start end', 'end start'],
+  });
 
+  // Rotate right on scrolling down and left on scrolling up
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, 3600]);
     const { t } = useTranslation();
   
   return (
-    <div id='about' className='h-[] bg-black text-white py-5 w-full grid place-content-center'>
-<div className=" sm:p-40  px-4 flex flex-col justify-center items-center">
+    <div id='about' className='h-[] bg-black text-white py-5 w-full grid place-content-center '>
+<div className=" sm:p-40 mt-10 px-4 flex flex-col justify-center items-center">
     <p data-aos="fade-up" className='text-8xl'>{t('about.ab')} <span className='text-red-600'>{t('about.us')} </span> </p>
     <p data-aos="fade-up"
     data-aos-duration="1600"
      className='sm:text-3xl text-2xl leading-loose tracking-wide py-10 text-center'>
     {t('about.para')} </p>
                             <motion.img
-                                className='h-[10vh] mt-20'
+                                className='h-[10vh] m-20'
                                 src="./bolt.png"
                                 alt="Rotating Bolt"
                                 initial={{ rotate: 0 }}
-                                animate={{ rotate: 360 }}
-                                transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
+                                style={{ rotate }}                                transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
                             />
 </div>
     </div>
